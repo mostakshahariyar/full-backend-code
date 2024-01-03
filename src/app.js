84ace5c10e372c804d799d07a0108204473ab888
mostakshahariyar <mostakshahariyar18@gmail.com>
2024-01-03 18:56:07 +0600

    all setup complete

--- src/app.js
+++ src/app.js
@@ -14,5 +14,15 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// routes 
+
+import userRouter from './routes/user.route.js';
+
+
+
+// routes declarations
+app.use("/api/v1/users", userRouter);
+
+
 
 export { app };
