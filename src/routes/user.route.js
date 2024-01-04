e2331af5c939de648bf6c241ed0fadf30c13bc0a
mostakshahariyar <mostakshahariyar18@gmail.com>
2024-01-04 15:33:12 +0600

    add custom middelware for logout

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { logOutUser, loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../minddlewares/multer.middleware.js";
+import { verifyJWT } from "../minddlewares/Auth.middleware.js";
 
 
 const userRouter = Router();
@@ -16,6 +17,14 @@ userRouter.route("/register").post(
                 }
         ]),
         registerUser
+)
+userRouter.route("/login").post(
+        loginUser
+)
+
+userRouter.route("/logout").post(
+        verifyJWT,
+        logOutUser
 )
 
 export default userRouter;
