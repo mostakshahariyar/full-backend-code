2070364efeb351eb1cb66632d06d97a7a64ebe46
mostakshahariyar <mostakshahariyar18@gmail.com>
2024-01-05 20:07:52 +0600

    current user and update details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -201,4 +201,63 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 });
 
-export { registerUser, loginUser, logOutUser, refreshAccessToken };
+
+// current password change
+const currentPasswordChange = asyncHandler(async (req, res) => {
+        const { oldPassword, newPassword, confirmPassword } = req.body;
+        if (!(newPassword === confirmPassword)) {
+                throw new ApiError(401, "password not same");
+        }
+        const user = await User.findById(req.user?._id);
+        const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+        if (!isPasswordCorrect) {
+                throw new ApiError(400, "Invalid old password");
+        }
+        user.password = newPassword;
+        await user.save({ validateBeforeSave: false });
+        return res.status(200).json(new ApiResponse(200, {}, "Password updated successfully"))
+})
+
+// current user
+const currentUser = asyncHandler(async (req, res) => {
+        return res.status(200)
+                .json(new ApiResponse(
+                        200,
+                        req.user,
+                        "current user successfully"
+                ))
+})
+
+// update ueser deltails
+const updateAccountDetails = asyncHandler(async (req, res) => {
+        const { fullName, email } = req.body;
+        if (!(fullName || email)) {
+                throw new ApiError(400, "full Name and eamil empty");
+        }
+        const user = User.findByIdAndUpdate(
+                req.user?._id,
+                {
+                        $set: {
+                                fullName,
+                                email
+                        }
+                },
+                { new: true }
+        ).select("-password -refreshToken")
+        return res.status(200)
+                .json(new ApiResponse(
+                        200,
+                        user,
+                        "Successfully updated"
+                ))
+})
+
+export {
+        registerUser,
+        loginUser,
+        logOutUser,
+        refreshAccessToken,
+        currentPasswordChange,
+        currentUser,
+        updateAccountDetails
+};
